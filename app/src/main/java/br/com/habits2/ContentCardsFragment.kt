a6875ef5b643c40db87d060a1680954f40796afa
Vitória Sá <vitoriafs98@outlook.com>
2022-10-07 22:24:24 -0300

    first commit

--- app/src/main/java/br/com/habits2/ContentCardsFragment.kt
+++ app/src/main/java/br/com/habits2/ContentCardsFragment.kt
@@ -8,8 +8,8 @@ import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val CONTEUDO = "conteudo"
+private const val PERFIL = "perfil"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,15 +17,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ContentCardsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var conteudo: String? = null
+    private var perfil: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            conteudo = it.getString(CONTEUDO)
+            perfil = it.getString(PERFIL)
         }
     }
 
@@ -46,13 +46,12 @@ class ContentCardsFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ContentCardsFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ContentCardsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(CONTEUDO, conteudo)
+                    putString(PERFIL, perfil)
                 }
             }
     }
--- app/src/main/java/br/com/habits2/data/CEP.kt
+++ app/src/main/java/br/com/habits2/data/CEP.kt
@@ -1,4 +1,14 @@
 package br.com.habits2.data
 
+import com.google.gson.annotations.SerializedName
+
 class CEP {
+
+    @SerializedName("cep") val cep: String;
+    @SerializedName("uf") val uf: String;
+    @SerializedName("complemento") val complemento: String;
+    @SerializedName("bairro") val bairro: String;
+    @SerializedName("rua") val rua: String;
+    @SerializedName("numero") val numero: String;
+
 }
